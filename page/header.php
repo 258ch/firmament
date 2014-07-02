b84fa08364f7937b415814a4483bc84cea45c2de
wizard <wizard.z@qq.com>
2014-07-02 18:26:59 +0800

    add favicon

--- page/header.php
+++ page/header.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="./css/bootstrap-theme.min.css" />
   <link rel="stylesheet" href="./css/style.css" />
   <script src="./js/jquery.min.js"></script>
+  <link rel="shortcut icon" href="/favicon.ico">
 </head>
 <body><div id="main" class="container">
 
